6f3602ab73db7fd4923745bce8be4485a4dfa25e
AnaliceMarques <analicemarquessl@gmail.com>
2023-05-05 18:03:22 -0300

    Criação do roteamento das entidades

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import { userRouter } from "./router/UserRouter";
+import { playlistRouter } from "./router/PlaylistRouter";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 app.listen(Number(process.env.PORT) || 3003, () => {
-    console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`)
-})
+  console.log(`Servidor rodando na porta ${Number(process.env.PORT) || 3003}`);
+});
+
+app.use("/users", userRouter);
+
+app.use("playlists", playlistRouter);
 
 app.get("/ping", (req, res) => {
-  res.send("Pong!")
-})
+  res.send("Pong!");
+});
